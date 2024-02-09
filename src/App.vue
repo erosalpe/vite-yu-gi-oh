6875ca0ba30c74710bb5482190951c7819bce311
erosalpe <81533597+erosalpe@users.noreply.github.com>
2024-02-09 15:04:18 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+  import nome from './components/blankComponent.vue'
+
+  export default{
+    components: {
+      nome,
+    }
+  }
+</script>
+
+
+
+
+
+<template>
+
+  <nome/>
+
+</template>
+
+
+
+
+
+<style>
+
+
+</style>
---
+++ src/components/blankComponent.vue
@@ -0,0 +1,36 @@
+<script>
+    export default {
+        name: "nome"
+    }
+</script>
+
+
+
+
+
+
+<template>
+  
+    <h1 class="d-flex justify-content-center">Benvenuto</h1>
+
+  
+
+
+</template>
+
+
+
+
+
+
+
+
+<style lang="scss" scoped>
+    h1{
+        color: Blue;
+    }
+
+
+
+
+</style>
